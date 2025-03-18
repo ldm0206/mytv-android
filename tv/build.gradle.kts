ef154ae2210835b35340f1d6dceacc7f5f72ce2d
ldm2060 <ldm200026@live.cn>
2025-03-18 19:17:25 +0800

    更新频道名称别名并提升版本号至1.4.0

--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.github.ldm0206.mytv"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 8
-        versionName = "1.3.3"
+        versionCode = 9
+        versionName = "1.4.0"
         vectorDrawables {
             useSupportLibrary = true
         }
