9db5c5b790eec1247e5bf5ea331be8472a62d0d1
ldm2060 <ldm200026@live.cn>
2025-03-21 20:16:14 +0800

    删除最新APK文件的处理逻辑，简化应用启动过程

--- tv/src/main/java/top/yogiczy/mytv/tv/MyTVApplication.kt
+++ tv/src/main/java/top/yogiczy/mytv/tv/MyTVApplication.kt
@@ -18,7 +18,7 @@ import io.sentry.android.core.SentryAndroid
 import top.yogiczy.mytv.core.data.AppData
 import top.yogiczy.mytv.core.data.utils.Globals
 import kotlin.system.exitProcess
-import java.io.File
+
 
 class MyTVApplication : Application(), ImageLoaderFactory {
     override fun onCreate() {
@@ -26,10 +26,6 @@ class MyTVApplication : Application(), ImageLoaderFactory {
 
         initSentry()
         crashHandle()
-        val latestFile = File(Globals.cacheDir, "latest.apk")
-        if (latestFile.exists()) {
-            latestFile.delete()
-        }
         AppData.init(applicationContext)
         UnsafeTrustManager.enableUnsafeTrustManager()
     }
