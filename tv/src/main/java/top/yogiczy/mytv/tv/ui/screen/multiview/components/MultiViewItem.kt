75e344c63cc6b4226d806d63a9315d8d93ec7718
ldm2060 <ldm200026@live.cn>
2025-03-26 15:49:20 +0800

    fix bug

--- tv/src/main/java/top/yogiczy/mytv/tv/ui/screen/multiview/components/MultiViewItem.kt
+++ tv/src/main/java/top/yogiczy/mytv/tv/ui/screen/multiview/components/MultiViewItem.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.VolumeOff
 import androidx.compose.runtime.Composable
