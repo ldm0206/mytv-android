c1cb2ff83139b8e5db6260dff6fc2bce5a4e6667
ldm2060 <ldm200026@live.cn>
2025-04-07 01:52:02 +0800

    在字幕设置界面中添加高度属性以优化布局

--- tv/src/main/java/top/yogiczy/mytv/tv/ui/screen/settings/subcategories/SettingsUiVideoPlayerSubtitleSettingsScreen.kt
+++ tv/src/main/java/top/yogiczy/mytv/tv/ui/screen/settings/subcategories/SettingsUiVideoPlayerSubtitleSettingsScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.lazy.grid.GridCells
