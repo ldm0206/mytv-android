6eaa9dabb52fadb60fb0449b7abdc25176087136
ldm2060 <ldm200026@live.cn>
2025-04-09 22:23:02 +0800

    重构 X5VideoPlayer，修复 prepare 方法并优化 TbsVideo 使用逻辑

--- tv/src/main/java/top/yogiczy/mytv/tv/ui/screensold/videoplayer/player/X5VideoPlayer.kt
+++ tv/src/main/java/top/yogiczy/mytv/tv/ui/screensold/videoplayer/player/X5VideoPlayer.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import kotlinx.coroutines.CoroutineScope
 import top.yogiczy.mytv.core.data.utils.Logger
 import top.yogiczy.mytv.core.data.utils.Loggable
+import top.yogiczy.mytv.core.data.entities.channel.ChannelLine
 
 import com.tencent.smtt.sdk.TbsVideo
 
@@ -12,16 +13,15 @@ class X5VideoPlayer(
     private val coroutineScope: CoroutineScope,
 ) : VideoPlayer(coroutineScope){
     private val logger = Logger.create("X5VideoPlayer")
-    private fun prepare(contentType: Int? = null) {
+
+    
+    override fun prepare(line: ChannelLine) {
         if(TbsVideo.canUseTbsPlayer(context)){
             logger.i("X5VideoPlayer: TbsVideo can be used")
+            TbsVideo.openVideo(context, line.playableUrl)
             triggerPrepared()
         } else {
             logger.i("X5VideoPlayer: TbsVideo cannot be used")
         }
-    }
-    
-    override fun prepare(line: ChannelLine) {
-        TbsVideo.openVideo(context, line.playableUrl)
     }
 }
